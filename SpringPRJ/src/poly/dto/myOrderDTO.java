c227aa75f68f2e50155af52c2866ec8c0ae8c602
kcyRepo <newshfkfk@gmail.com>
2020-07-27 16:57:20 +0900

    myOrderDTO

---
+++ SpringPRJ/src/poly/dto/myOrderDTO.java
@@ -0,0 +1,97 @@
+package poly.dto;
+
+public class myOrderDTO {
+
+	private String user_no;
+	private String user_name;
+	private String user_id;
+	private String user_pwd;
+	private String user_signdt;
+
+	private String user_menu;
+	private String user_price;
+	private String user_odt;
+	private String user_quantity;
+	private String user_ttpc;
+
+	public String getUser_quantity() {
+		return user_quantity;
+	}
+
+	public void setUser_quantity(String user_quantity) {
+		this.user_quantity = user_quantity;
+	}
+
+	public String getUser_ttpc() {
+		return user_ttpc;
+	}
+
+	public void setUser_ttpc(String user_ttpc) {
+		this.user_ttpc = user_ttpc;
+	}
+
+	public String getUser_no() {
+		return user_no;
+	}
+
+	public void setUser_no(String user_no) {
+		this.user_no = user_no;
+	}
+
+	public String getUser_name() {
+		return user_name;
+	}
+
+	public void setUser_name(String user_name) {
+		this.user_name = user_name;
+	}
+
+	public String getUser_id() {
+		return user_id;
+	}
+
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+
+	public String getUser_pwd() {
+		return user_pwd;
+	}
+
+	public void setUser_pwd(String user_pwd) {
+		this.user_pwd = user_pwd;
+	}
+
+	public String getUser_signdt() {
+		return user_signdt;
+	}
+
+	public void setUser_signdt(String user_signdt) {
+		this.user_signdt = user_signdt;
+	}
+
+	public String getUser_menu() {
+		return user_menu;
+	}
+
+	public void setUser_menu(String user_menu) {
+		this.user_menu = user_menu;
+	}
+
+	public String getUser_price() {
+		return user_price;
+	}
+
+	public void setUser_price(String user_price) {
+		this.user_price = user_price;
+	}
+
+	public String getUser_odt() {
+		return user_odt;
+	}
+
+	public void setUser_odt(String user_odt) {
+		this.user_odt = user_odt;
+	}
+
+}
